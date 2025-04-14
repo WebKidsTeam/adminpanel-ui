780ffce369ee752d08d54a5a2bafa944888ba75a
Dima Hirchyk <dimzuk04@gmail.com>
2025-04-14 15:59:56 +0300

    docs

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-pattern': [
+      2,
+      'always',
+      /^(\w+)\((WK-\d+)\):\s.+$/,
+    ],
+    'header-max-length': [2, 'always', 100],
+  },
+};
